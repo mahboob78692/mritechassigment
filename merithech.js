7a7a887a03dbb05f8cabcae740abea36db66afdb
MD MAHBOOB ALAM <130448065+mahboob78692@users.noreply.github.com>
2026-04-16 15:43:28 +0530

    Add files via upload

---
+++ merithech.js
@@ -0,0 +1,19 @@
+// Add slight mouse tilt effect
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateX = -(y / rect.height - 0.5) * 10;
+    const rotateY = (x / rect.width - 0.5) * 10;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotate(0) scale(1)";
+  });
+});
